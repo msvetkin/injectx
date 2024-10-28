27cc8d6dd767cb97337da742d41f58a5cb596a6c
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2024-10-28 22:25:29 +0100

    feat(stdext): ranges aliases

---
+++ src/stdext/include/injectx/stdext/ranges/aliases.hpp
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: Copyright 2024 Mikhail Svetkin
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace injectx::stdext {
+
+namespace ranges::views {}
+
+namespace r = ranges;
+namespace rv = r::views;
+
+}  // namespace injectx::stdext
