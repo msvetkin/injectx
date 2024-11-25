fd467298953252213aca4bf3bfae40069fc9cc20
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2024-11-25 15:47:45 +0100

    u(stdext): task re-format

--- src/stdext/include/injectx/stdext/coro/task.hpp
+++ src/stdext/include/injectx/stdext/coro/task.hpp
@@ -5,35 +5,34 @@
 
 #include "injectx/stdext/coro/promise.hpp"
 #include "injectx/stdext/expected.hpp"
-#include "injectx/stdext/monadics.hpp"
 
 #include <string_view>
 #include <utility>
 
 namespace injectx::stdext::coro {
 
-template <typename T, template <typename> typename Promise>
+template<typename T, template<typename> typename Promise>
 class [[nodiscard]] task;
 
 namespace details::_task {
 
-template <typename T>
+template<typename T>
 class promise_base : public promise_storage<T> {
  public:
-  template <typename U = T>
+  template<typename U = T>
   void return_value(U&& value) noexcept {
     promise_storage<T>::set(std::forward<U>(value));
   }
 };
 
-template <>
+template<>
 class promise_base<void> : public promise_storage<void> {
  public:
   void return_void() const noexcept {
   }
 };
 
-template <typename TaskPromise>
+template<typename TaskPromise>
 class [[nodiscard]] Awaitable {
  public:
   using handle_type = stdext::coroutine_handle<TaskPromise>;
@@ -46,7 +45,7 @@ class [[nodiscard]] Awaitable {
     return !handle_ || handle_.done();
   }
 
-  template <typename Promise>
+  template<typename Promise>
   auto await_suspend(
       stdext::coroutine_handle<Promise> awaitingHandle) noexcept {
     return handle_.promise().suspend(awaitingHandle, handle_);
@@ -60,16 +59,18 @@ class [[nodiscard]] Awaitable {
   handle_type handle_;
 };
 
-template <typename T>
-class promise : public promise_base<T>, public promise_continuation {
+template<typename T>
+class promise : public promise_base<T>,
+                public promise_continuation {
  public:
   task<T, promise> get_return_object() noexcept;
 };
 
 }  // namespace details::_task
 
-template <typename T,
-          template <typename> typename Promise = details::_task::promise>
+template<
+    typename T,
+    template<typename> typename Promise = details::_task::promise>
 class [[nodiscard]] task {
  public:
   using promise_type = Promise<T>;
@@ -116,13 +117,14 @@ class [[nodiscard]] task {
     using Expected = stdext::expected<void, std::string_view>;
 
     return is_ready() | and_then([this](const bool ready) -> Expected {
-      if (ready) {
-        return stdext::unexpected{"coroutine is in the final suspend point"};
-      }
+             if (ready) {
+               return stdext::unexpected{
+                   "coroutine is in the final suspend point"};
+             }
 
-      handle_.resume();
-      return {};
-    });
+             handle_.resume();
+             return {};
+           });
   }
 
   [[nodiscard]] promise_type& promise() noexcept {
@@ -139,7 +141,7 @@ class [[nodiscard]] task {
 
 namespace details::_task {
 
-template <typename T>
+template<typename T>
 [[nodiscard]] task<T, promise> promise<T>::get_return_object() noexcept {
   return task<T, promise>{
       stdext::coroutine_handle<promise>::from_promise(*this)};
