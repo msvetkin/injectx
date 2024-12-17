1589b843f5676b9407a93d9031811cb641d4df37
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2024-12-18 00:40:21 +0100

    refactor(stdext): port over pipe_fn and_then

--- src/stdext/include/injectx/stdext/monadics/and_then.hpp
+++ src/stdext/include/injectx/stdext/monadics/and_then.hpp
@@ -9,16 +9,21 @@ namespace injectx::stdext {
 
 namespace monadics::details::_and_then {
 
+template<typename Callback>
 struct action {
-  template<typename T, typename F>
-  [[nodiscard]] static constexpr decltype(auto) invoke(T &&t, F &&f) noexcept
-    requires requires { and_then_as(std::forward<T>(t), std::forward<F>(f)); }
+  Callback callback;
+
+  template<typename T>
+  [[nodiscard]] friend constexpr auto operator|(T &&t, action &&a) noexcept
+    requires requires {
+      and_then_as(std::forward<T>(t), std::forward<Callback>(a.callback));
+    }
   {
-    return and_then_as(std::forward<T>(t), std::forward<F>(f));
+    return and_then_as(std::forward<T>(t), std::forward<Callback>(a.callback));
   }
 };
 
-using fn = monadics::pipe<action>;
+using fn = monadics::pipe_fn<action>;
 
 }  // namespace monadics::details::_and_then
 
